61f5184e4e73e7baf44197aabf0ca8a2657ccc44
proldanp <proldanp@example.com>
2026-04-04 23:04:35 +0200

    major: update scraping

---
+++ test_nico.py
@@ -0,0 +1,7 @@
+import json
+from scraping.transfermarkt_players import TransfermarktPlayersScraper
+
+scraper = TransfermarktPlayersScraper()
+player = scraper.scrape_player_details('486031')
+print("Signed Date:", player.signed_date)
+print("Contract End:", player.contract_end_date)
